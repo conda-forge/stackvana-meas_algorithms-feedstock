967ea19549065f3d04bbc38211648c5603a2d65f
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-02-01 07:44:07 -0600

    Update stackvana-build-local.sh

--- recipe/stackvana-build-local.sh
+++ recipe/stackvana-build-local.sh
@@ -129,7 +129,7 @@ echo " "
 
 echo "Running eups install..."
 {
-    export SCONSFLAGS=${SCONSFLAGS}" --no-test"
+    export SCONSFLAGS=${SCONSFLAGS}" --no-tests"
     eups distrib install -v -t ${LSST_DM_TAG} $@
 } || {
     _report_errors
